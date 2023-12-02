f13564c77abd11ed6f923debbb05f09b74c9279d
张浩阳 <haoyangmob@163.com>
2023-12-02 11:12:28 +0800

    addedd paralle axis

--- Pages/test1.py
+++ Pages/test1.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_echarts import st_echarts
 import pandas as pd
+import seaborn as sns
 
 # 设置页面布局为宽屏模式，充满整个屏幕宽度
 st.set_page_config(layout="wide", page_icon=None,
@@ -31,6 +32,11 @@ selected_categories = set(node_categories)
 
 selected_nodes = []
 with left_column:
+    graph_type = st.radio(
+        "Choose Graph Type",
+        ('ECharts Graph', 'Parallel Axis Plot')
+    )
+        
     st.subheader("Quick Select Suspect")
     for node_id in special_node_ids:
         if st.checkbox(node_id, key=node_id):
@@ -87,119 +93,157 @@ category_colors = {
     "Uncategorized": "#eca680"
 }
 
+# Load and prepare data for Parallel Axis Plot
+def prepare_parallel_axis_data():
+    # Load the dataset
+    data_df = pd.read_csv('Dataset/MC1/community/community_stats.csv')
+
+    # Extract column names for axis labels
+    axis_labels = data_df.columns.tolist()[1:]  # Assuming first column is an index or identifier
+
+    # Prepare data for ECharts format
+    data = data_df.values.tolist()
+
+    return axis_labels, data
+
+# Configuration for parallel axis plot
+def get_parallel_axis_option(axis_labels, data):
+    # Define axis configuration based on the labels
+    axes = [{"dim": i, "name": label} for i, label in enumerate(axis_labels)]
+
+    option = {
+        "parallelAxis": axes,
+        "series": {
+            "type": "parallel",
+            "data": data
+        }
+    }
+    return option
+
 
 # Main area for displaying the graph
 with mid_column:
-    if selected_nodes and selected_link_types and selected_categories:
-        neighbors_set = get_neighbors(
-            selected_nodes, links_df, selected_link_types, selected_categories)
-        filtered_df = links_df[(links_df['source'].isin(neighbors_set)) & (
-            links_df['target'].isin(neighbors_set)) & (links_df['type'].isin(selected_link_types))]
+    if graph_type == 'ECharts Graph':
+        if selected_nodes and selected_link_types and selected_categories:
+            neighbors_set = get_neighbors(
+                selected_nodes, links_df, selected_link_types, selected_categories)
+            filtered_df = links_df[(links_df['source'].isin(neighbors_set)) & (
+                links_df['target'].isin(neighbors_set)) & (links_df['type'].isin(selected_link_types))]
 
-        echarts_nodes = [
-            {
-                "name": node,
-                # Increase size for special nodes
-                "symbolSize": 10 if node in special_node_ids else 5,
-                "draggable": True,
-                "category": node_types.get(node, "Unknown"),
-                # Set shape to star for special nodes
-                "symbol": 'rect' if node in special_node_ids else 'circle',
-            }
-            for node in neighbors_set
-        ]
-
-        echarts_links = filtered_df.apply(
-            lambda row: {"source": row['source'],
-                         "target": row['target'], "value": row['type']}
-            if row['weight'] > conf_threshold else None,
-            axis=1
-        ).dropna().tolist()
-
-        # 定义不同类别的节点样式
-        categories = [
-            {"name": "person", "itemStyle": {"color": "#df493f"}},
-            {"name": "political_organization", "itemStyle": {"color": "#f9d580"}},
-            {"name": "organization", "itemStyle": {"color": "#e4a2b8"}},
-            {"name": "event", "itemStyle": {"color": "#54beaa"}},
-            {"name": "company", "itemStyle": {"color": "#fcf1f0"}},
-            {"name": "location", "itemStyle": {"color": "#b0d992"}},
-            {"name": "vessel", "itemStyle": {"color": "#99b9e9"}},
-            {"name": "movement", "itemStyle": {"color": "#af8fd0"}},
-            {"name": "Uncategorized", "itemStyle": {"color": "#eca680"}}
-        ]
-
-        # ECharts directed graph configuration
-        option = {
-            "backgroundColor": '#FFFFFF',  # 设置背景色为白色
-            "tooltip": {},
-            "legend": {
-                "data": list(map(lambda c: c['name'], categories)),
-                "selectedMode": True  # 禁止legend的默认点击行为
-            },
-            "series": [
+            echarts_nodes = [
                 {
-                    "type": "graph",
-                    "layout": "force",
-                    "symbolSize": 10,
-                    "focusNodeAdjacency": True,
-                    "roam": True,
+                    "name": node,
+                    # Increase size for special nodes
+                    "symbolSize": 10 if node in special_node_ids else 5,
                     "draggable": True,
-                    "focusNodeAdjacency": False,  # 当点击一个节点时，高亮显示与其相连的边和节点
-                    "label": {
-                        "show": True,
-                        "position": 'right',  # 可以根据实际情况调整标签位置
-                        "color": "black"  # 设置节点文字颜色
-                    },
-                    "categories": categories,
-                    "edgeSymbol": ["none", "arrow"],
-                    "edgeSymbolSize": [0, 10],  # 根据需要调整箭头的大小
-                    "nodes": echarts_nodes,
-                    "links": echarts_links,
-                    "lineStyle": {
-                        "opacity": 0.9,
-                        "width": 2,
-                        "curveness": 0.1
-                    },
-                    # 在这里增加了一个itemStyle属性来增加节点的大小
-                    "itemStyle": {
-                        "normal": {
-                            "borderWidth": 0,  # 设置边框宽度，用于放大效果
-                            "borderColor": '#fff'
-                        }
-                    },
-                    "force": {
-                        "layoutAnimation": False,
-                        "repulsion": 1000,
-                        "edgeLength": 10,
-                        "gravity": 0.5
-                    }
+                    "category": node_types.get(node, "Unknown"),
+                    # Set shape to star for special nodes
+                    "symbol": 'rect' if node in special_node_ids else 'circle',
                 }
-            ],
-
-            "graphic": [
-                {
-                    "type": "rect",
-                    "z": 100,
-                    "left": 'center',
-                    "top": 'center',
-                    "shape": {
-                        "width": 1000,  # 画布宽度
-                        "height": 1000  # 画布高度
-                    },
-                    "style": {
-                        "fill": 'none',
-                        "shadowBlur": 10,
-                        "shadowOffsetX": 5,
-                        "shadowOffsetY": 5,
-                        "shadowColor": 'rgba(0,0,0,0.3)'
-                    }
-                }
+                for node in neighbors_set
             ]
-        }
 
-        # Display the graph using streamlit_echarts
-        st_echarts(options=option, height="1000px")
+            echarts_links = filtered_df.apply(
+                lambda row: {"source": row['source'],
+                            "target": row['target'], "value": row['type']}
+                if row['weight'] > conf_threshold else None,
+                axis=1
+            ).dropna().tolist()
+
+            # 定义不同类别的节点样式
+            categories = [
+                {"name": "person", "itemStyle": {"color": "#df493f"}},
+                {"name": "political_organization", "itemStyle": {"color": "#f9d580"}},
+                {"name": "organization", "itemStyle": {"color": "#e4a2b8"}},
+                {"name": "event", "itemStyle": {"color": "#54beaa"}},
+                {"name": "company", "itemStyle": {"color": "#fcf1f0"}},
+                {"name": "location", "itemStyle": {"color": "#b0d992"}},
+                {"name": "vessel", "itemStyle": {"color": "#99b9e9"}},
+                {"name": "movement", "itemStyle": {"color": "#af8fd0"}},
+                {"name": "Uncategorized", "itemStyle": {"color": "#eca680"}}
+            ]
+
+            # ECharts directed graph configuration
+            option = {
+                "backgroundColor": '#FFFFFF',  # 设置背景色为白色
+                "tooltip": {},
+                "legend": {
+                    "data": list(map(lambda c: c['name'], categories)),
+                    "selectedMode": True  # 禁止legend的默认点击行为
+                },
+                "series": [
+                    {
+                        "type": "graph",
+                        "layout": "force",
+                        "symbolSize": 10,
+                        "focusNodeAdjacency": True,
+                        "roam": True,
+                        "draggable": True,
+                        "focusNodeAdjacency": False,  # 当点击一个节点时，高亮显示与其相连的边和节点
+                        "label": {
+                            "show": True,
+                            "position": 'right',  # 可以根据实际情况调整标签位置
+                            "color": "black"  # 设置节点文字颜色
+                        },
+                        "categories": categories,
+                        "edgeSymbol": ["none", "arrow"],
+                        "edgeSymbolSize": [0, 10],  # 根据需要调整箭头的大小
+                        "nodes": echarts_nodes,
+                        "links": echarts_links,
+                        "lineStyle": {
+                            "opacity": 0.9,
+                            "width": 2,
+                            "curveness": 0.1
+                        },
+                        # 在这里增加了一个itemStyle属性来增加节点的大小
+                        "itemStyle": {
+                            "normal": {
+                                "borderWidth": 0,  # 设置边框宽度，用于放大效果
+                                "borderColor": '#fff'
+                            }
+                        },
+                        "force": {
+                            "layoutAnimation": False,
+                            "repulsion": 1000,
+                            "edgeLength": 10,
+                            "gravity": 0.5
+                        }
+                    }
+                ],
+
+                "graphic": [
+                    {
+                        "type": "rect",
+                        "z": 100,
+                        "left": 'center',
+                        "top": 'center',
+                        "shape": {
+                            "width": 1000,  # 画布宽度
+                            "height": 1000  # 画布高度
+                        },
+                        "style": {
+                            "fill": 'none',
+                            "shadowBlur": 10,
+                            "shadowOffsetX": 5,
+                            "shadowOffsetY": 5,
+                            "shadowColor": 'rgba(0,0,0,0.3)'
+                        }
+                    }
+                ]
+            }
+
+            # Display the graph using streamlit_echarts
+            st_echarts(options=option, height="1000px")
+    elif graph_type == 'Parallel Axis Plot':
+        # Prepare data for parallel axis plot
+        axis_labels, data = prepare_parallel_axis_data()
+
+        # Get the parallel axis plot configuration
+        parallel_axis_option = get_parallel_axis_option(axis_labels, data)
+
+        # Display the parallel axis plot using streamlit_echarts
+        st_echarts(options=parallel_axis_option, height="1000px")
+
 
 with right_column:
     st.subheader("Edeg Statistics")
