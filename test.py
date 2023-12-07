61e6a36e0f215f7efc49ce40ba5187140acd16d7
yuxua24 <1214076062@qq.com>
2023-12-07 08:34:31 +0800

    上传company.csv

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 import pandas as pd
 
-# 读取包含数据的DataFrame
-df = pd.read_csv('Dataset/MC3/nodes.csv')
+# 读取CSV文件
+df = pd.read_csv('Dataset/MC3/2.csv')
+
+# 计算平均收入
+# 使用numpy的where函数来进行条件选择
+# 如果BOCnt不为0，使用revenue_omu / BOCnt，否则使用revenue_omu / CCCnt
+df['average_income'] = df.apply(
+    lambda row: row['revenue_omu'] / row['BOCnt'] if row['BOCnt'] != 0 else row['revenue_omu'] / row['CCCnt'], 
+    axis=1
+)
+
+# 将结果保存到新的CSV文件
+df.to_csv('Dataset/MC3/3.csv', index=False)
 
 
-# 使用groupby和count来统计国家为空值的各种类型的数量
-result = df[df['country'].isnull()].groupby('type').size().reset_index(name='count')
 
-# 打印结果
-print(result)
