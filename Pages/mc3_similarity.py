44033df9d9a509081dc89ef92f5844f2ca544be7
Z_JiuRi <2061362927@qq.com>
2023-12-07 22:12:11 +0800

    更新MC3相似度页面功能

--- Pages/mc3_similarity.py
+++ Pages/mc3_similarity.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 st.set_page_config(page_title="FishEye", layout="wide",
                    page_icon=None, initial_sidebar_state="collapsed")
 
@@ -7,13 +8,13 @@ with st.container():
     col_select, col1, col2, col3, col4, col5 = st.columns([2, 1, 1, 1, 1, 1])
 
     with col_select:
-        select_box = st.selectbox(
-            "Suspicious nodes", ["Option 1", "Option 2", "Option 3"])
+        if 'sus_nodes' in st.session_state:
+            select_box = st.selectbox(
+                "Suspicious nodes", list(st.session_state['sus_nodes']))
         st.button('SHOW SIMILAR COMPANY')
 
     with col1:
-        slider1 = st.slider("Company Size", min_value=0.0,
-                            max_value=1.0, value=0.0, step=0.01)
+        slider1 = st.slider("Company Size", 0.0, 1.0, step=0.01)
         slider1_2 = st.slider("", -1.0, 0.0, value=0.0,
                               step=0.01, key='slider1_2')
     with col2:
@@ -40,10 +41,14 @@ left_col, line_col, right_col = st.columns([1, 0.01, 1])
 
 with left_col:
     st.subheader("Selected Node")
+    st.button('REMOVE')
 
 with line_col:
-    st.markdown('<div style="height: 1000px; width: 1px; background-color: #d3d3d3;"></div>',
+    st.markdown('<div style="height: 600px; width: 1px; background-color: #d3d3d3;"></div>',
                 unsafe_allow_html=True)
 
 with right_col:
     st.subheader("Similar Node")
+    st.button('EXPAND')
+
+st.markdown("---")
