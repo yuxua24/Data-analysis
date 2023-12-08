51eee8d52630e9a330f4c09e8de85a4bb8e9d931
Z_JiuRi <2061362927@qq.com>
2023-12-08 19:29:38 +0800

    相似度计算

--- Pages/mc3_similarity.py
+++ Pages/mc3_similarity.py
@@ -13,7 +13,8 @@ show_similar = False
 similar_nodes = set()
 
 with st.container():
-    col_select, col1, col2, col3, col4, col5 = st.columns([2, 1, 1, 1, 1, 1])
+    col_select, col1, col2, col3, col4, col5, col6 = st.columns(
+        [2, 1, 1, 1, 1, 1, 1])
 
     with col1:
         slider1 = st.slider("Company Size", 0.0, 1.0, step=0.01)
@@ -35,7 +36,10 @@ with st.container():
         slider5 = st.slider("Same Staff", 0.0, 1.0, step=0.01)
         slider5_2 = st.slider("", -1.0, 0.0, value=0.0,
                               step=0.01, key='slider5_2')
-
+    with col6:
+        slider6 = st.slider("Company_type", 0.0, 1.0, step=0.01)
+        slider6_2 = st.slider("", -1.0, 0.0, value=0.0,
+                              step=0.01, key='slider6_2')
     with col_select:
         node_chosen = st.selectbox(
             "Suspicious nodes", list(st.session_state['sus_nodes3']))
@@ -46,14 +50,25 @@ with st.container():
             global show_similar
             global similar_nodes
             show_similar = True
-            cols = ['id','product_services','revenue_omu','type','country','company_type','company_size','clothing','furniture','groceries','logistics','machinery','management','metals','miscellaneous','pharmaceutical','plastics','food','seafood','missing']
+            cols = ['revenue_omu', 'country', 'company_type', 'company_size', 'clothing', 'furniture', 'groceries',
+                    'logistics', 'machinery', 'management', 'metals', 'miscellaneous', 'pharmaceutical', 'plastics', 'food', 'seafood', 'missing']
             res = dict()
             for idx1, row1 in nodes1.iterrows():
-                if row1['id']==node_chosen:#先找到选择的节点
+                if row1['id'] == node_chosen:  # 先找到选择的节点
                     for idx, row in nodes1:
-                        sum=0.0
-                        for col in nodes1.columns:
-                            sum+=0.0
+                        sum = 0.0
+                        for col in cols:
+                            if col == 'company_size':
+                                sum += slider1 if row[col] == row1[col] else slider1_2
+                                cols = cols.remove(col)
+                            elif col == 'country':
+                                sum += slider2 if row[col] == row1[col] else slider2_2
+                                cols = cols.remove(col)
+                            elif col == 'revenue_omu':
+                                sum += slider4 if row[col] == row1[col] else slider4_2
+                                cols = cols.remove(col)
+                            else:
+                                cols = cols.remove()
                 break
 
         st.button('SHOW SIMILAR NODES', on_click=handle_show)
@@ -61,7 +76,7 @@ with st.container():
 
 st.markdown("---")
 
-left_col, line_col, right_col = st.columns([1, 0.01, 1])
+left_col, right_col = st.columns([1, 1])
 
 with left_col:
     st.subheader("Selected Node")
@@ -70,19 +85,15 @@ with left_col:
         st.session_state['sus_nodes3'].discard(node_chosen)
     st.button('REMOVE', on_click=handle_remove)
 
-    for index, row in nodes.iterrows():
-        if row['id'] == node_chosen:
-            details = ''
-            for col in nodes.columns:
-                st.markdown(
-                    f"<span style='font-size: 20px; color: #000000;'><b>{col}:</b></span>", unsafe_allow_html=True)
-                st.markdown(row[col])
-            break
-
-
-with line_col:
-    st.markdown('<div style="height: 750px; width: 1px; background-color: #d3d3d3;"></div>',
-                unsafe_allow_html=True)
+    with st.expander(node_chosen, expanded=True):
+        for index, row in nodes.iterrows():
+            if row['id'] == node_chosen:
+                details = ''
+                for col in nodes.columns:
+                    st.markdown(
+                        f"<span style='font-size: 20px; color: #000000;'><b>{col}:</b></span>", unsafe_allow_html=True)
+                    st.markdown(row[col])
+                break
 
 with right_col:
     st.subheader("Similar Nodes")
@@ -90,6 +101,5 @@ with right_col:
     def handle_expand():
         pass
     st.button('EXPAND', on_click=handle_expand)
-
-
-st.markdown("---")
+    with st.expander("查看更多信息"):
+        st.write("这里是详细信息...")
